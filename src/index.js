785cf876698249b2c182ded03bec64ef574ea7e3
lowresboots <lowresboots@gmail.com>
2024-12-07 04:35:31 -0500

    Added SVG background icon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './styles/main.css';
+import './styles/background.css';
+import bootIcon from './assets/boot.svg';
+
+const boot = new Image();
+boot.src = bootIcon;
+boot.classList.add('background-boot');
+document.body.appendChild(boot);
 
 console.log('Bootdo initialization successful!');
